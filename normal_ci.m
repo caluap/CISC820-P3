c7d1a4a92e53d87f5514d6e3a99837d5b8968928
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2021-11-06 17:34:47 -0400

    Create normal_ci.m

---
+++ normal_ci.m
@@ -0,0 +1,36 @@
+function normal_ci(n, alpha)
+
+    if alpha == 0.01
+        ep0 = 2.58;
+    elseif alpha == 0.05
+        ep0 = 1.96
+    elseif alpha == 0.1
+        ep0 = 1.64
+    elseif alpha == 0.25
+        ep0 = 1.15
+    else
+        error('Alpha must be in [0.01, 0.05, 0.1, 0.25].');
+    end
+    
+
+    for k = [10, 100, 1000, 10000]
+        successes = 0;
+        for i=1:n
+            sample = sample_bernoulli(k, 0.5);
+
+            Xbar = avg(sample);
+            sig = std(sample);
+            ep = sig * ep0 / sqrt(k);
+            sample_ci = [Xbar - ep, Xbar + ep];
+
+            % sample_ci = confidence_interval( k, .05, avg(sample) );
+            if sample_ci(1) <= 0.5 && 0.5 <= sample_ci(2)
+                successes = successes + 1;
+            end
+        end
+        success_rate = successes / n;
+        fprintf('Success rate = %f for k = %d\n', success_rate, k);
+
+    end
+
+end
