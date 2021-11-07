ac566332d9dcf0cd9b49c09dff7e4b158a648baf
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2021-11-07 16:30:46 -0500

    some normal tests

--- main.m
+++ main.m
@@ -2,32 +2,68 @@
 exp_size = 10000;
 % comment
 
+bernoulli = true;
+normal = true;
+uniform = false;
+
+
 for func_num = 1:10
     
-    fprintf('\n\n\n##### This if Function %u #####', func_num);
+    fprintf('\n\n\n##### Tests for Function %u #####', func_num);
     
     % Bernoulli
-    fprintf('\n\nBernoulli distribution');
-    for theta = [0, 0.001, 0.01, 0.1, 0.25, 0.3, 0.5, 0.7, 0.75, 0.9, 0.99, 0.999, 1]
-        for k = [1000, 10000]
-            linweis_hits = 0;
-            for i = 1:exp_size
-                sample = sample_bernoulli(k, theta);
-                [a b] = ci(sample, func_num);
-                if theta >= a && theta <= b
-                    linweis_hits = linweis_hits + 1;
+    if bernoulli
+        fprintf('\n\nBernoulli distribution');
+        for theta = [0, 0.001, 0.01, 0.1, 0.25, 0.3, 0.5, 0.7, 0.75, 0.9, 0.99, 0.999, 1]
+            for k = [100, 1000, 10000]
+                linweis_hits = 0;
+                for i = 1:exp_size
+                    sample = sample_bernoulli(k, theta);
+                    [a b] = ci(sample, func_num);
+                    if theta >= a && theta <= b
+                        linweis_hits = linweis_hits + 1;
+                    end
                 end
+                fprintf('\ntheta=%g \t Hits=%g \t k=%u', theta, linweis_hits/exp_size, k );
             end
-            fprintf('\ntheta=%g \t Hits=%g \t k=%u', theta, linweis_hits/exp_size, k );
+            fprintf('\n');
         end
-        fprintf('\n');
     end
     
-    % Normal
-    fprintf('\n\nNormal distribution');
-    sample = sample_normal(k, 0.1, 0.5);
+    if normal
+        % Normal
+        fprintf('\n\nNormal distribution');
+
+        mus     = [0.5, 0.50, 0.10, 0.90, 4.0];
+        sigmas  = [0.1, 0.02, 0.02, 0.02, 0.1];
+ 
+        for i=1:length(mus)
+            mu = mus(i);
+            sigma = sigmas(i);
+            for k = [100, 1000, 10000]            
+                linweis_hits = 0;
+                avg_a = 0;
+                avg_b = 0;
+                for i = 1:exp_size
+                    sample = sample_normal(k, sigma, mu);
+                    [a b] = ci(sample, func_num);
+                    avg_a = avg_a + a/exp_size;
+                    avg_b = avg_b + b/exp_size;
+                    if mu >= a && mu <= b
+                        linweis_hits = linweis_hits + 1;
+                    end
+                end
+                fprintf('\nmu=%.2f   sigma=%.2f   Hits=%.2f   avg_a=%.2f   avg_b=%.2f   k=%u', mu, sigma, linweis_hits/exp_size, avg_a, avg_b, k );
+            end            
+            fprintf('\n');
+        end
+    end
     
     % Uniform
-    fprintf('\n\nUniform distribution');
-    sample = sample_uniform(k, 0, 1);
+    if uniform
+        fprintf('\n\nUniform distribution');
+        sample = sample_uniform(k, 0, 1);
+    end
 end
+
+fprintf('\n\n\nDone!\n');
